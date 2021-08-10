9a073866c690147aafce97b4421963383315a970
Enyel Jan Cuadro <63972293+whoisnyl@users.noreply.github.com>
2021-08-10 17:38:09 +0800

    graphql test

--- server.js
+++ server.js
@@ -1,29 +1,79 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
-const customerRoutes = require("./routes/customers");
-const clientRoutes = require("./routes/clients");
-const invoiceRoutes = require("./routes/invoices");
+const schema = buildSchema(`
+  type Address {
+    addressline1: String
+    addressline2: String
+    city: String
+    stateOrProvince: String
+    postalcode: String
+    notes: String
+    unit: String
+    country: String
+  }
 
-server.use(middlewares);
-server.use(jsonServer.bodyParser);
+  input AddressInput {
+    addressline1: String
+    addressline2: String
+    city: String
+    stateOrProvince: String
+    postalcode: String
+    notes: String
+    unit: String
+    country: String
+  }
 
-/*
- * NextPay Customer's Endpoints
- * ============================
- */
+  type Customer {
+    name: String
+    uuid: String
+    businessAddress: Address
+    mobileNumber: String
+    email: String
+    contactPerson: String
+    created_at: String
+    updated_at: String
+  }
 
-server.use("/nextpay", customerRoutes);
-server.use("/nextpay", clientRoutes);
-server.use("/nextpay", invoiceRoutes);
+  input CustomerInput {
+    name: String!
+    email: String!
+    mobileNumber: String
+    contactPerson: String
+    businessAddressInput: AddressInput
+  }
 
-// unknown routes
-server.use((req, res) => {
-  throw new Error("Could not find this route.");
+  input EditCustomerInput {
+    name: String!
+    email: String!
+    mobileNumber: String
+    contactPerson: String
+    businessAddressInput: AddressInput
+    uuid: String
+  }
+
+  type Query {
+    findOneCustomer(uuid: String!): Customer
+  }
+
+  type Mutation {
+    createCustomer(customerInput: CustomerInput, addressInput: AddressInput): Customer
+    editCustomer(customerInput: EditCustomerInput): Customer
+    deleteCustomer(uuid: String!): Customer
+  }
+`);
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000...");
 });
-
-server.use(router);
-server.listen(port);
